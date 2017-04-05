6ebae63130d92293025efb22b14eae41f70552da
wpatter6 <wpatter6@gmail.com>
2017-04-05 14:08:49 -0600

    Update RedditChart.user.js

--- RedditChart.user.js
+++ RedditChart.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Some data analysis for reddit
-// @author       You
+// @author       wpatter6
 // @match        *://*.reddit.com/*
 // @grant        none
 // ==/UserScript==
